0004f18b78b8223e0371722bdd6231d0f6848513
hereiam123 <bdemaio1@gmail.com>
2019-09-27 21:34:41 -0400

    Added nebular theme styling, and now have a functioning game. Adding in PWA access in the future

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,9 +3,17 @@ import { Component, OnInit, Input } from "@angular/core";
 @Component({
   selector: "app-square",
   template: `
-    <button>{{ value }}</button>
+    <button nbButton *ngIf="!value">{{ value }}</button>
+
+    <button nbButton hero status="success" *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+
+    <button nbButton hero status="info" *ngIf="value == 'O'">
+      {{ value }}
+    </button>
   `,
-  styles: ["button{height:100%; width:100%;}"]
+  styles: ["button {height: 100%; width: 100%;}"]
 })
 export class SquareComponent {
   @Input() value: "X" | "O";
